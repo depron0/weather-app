4f84855664f62b2134dfaa6c4e04e142689f9207
depron0 <sceneronja@gmail.com>
2023-02-03 23:24:38 +0100

    minor changes

--- src/api.js
+++ src/api.js
@@ -12,5 +12,5 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
-export const WEATHER_API_URL_KEY = process.env.REACT_APP_API_KEY_TWO
+export const WEATHER_API_KEY = process.env.REACT_APP_API_KEY_TWO
 
