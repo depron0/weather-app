749edc43679f6770c8a8fa4f5c794b9285fb7f87
depron0 <sceneronja@gmail.com>
2023-02-03 23:19:32 +0100

    added .env file, encountered new error with API keys and fs - not resolved yet

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import Search from './components/search/index'
 import CurrentWeather from './components/current-weather/index'
 import Forecast from './components/forecast/index'
-import { WEATHER_API_URL, WEATHER_API_URL_KEY } from './api'
+import { WEATHER_API_URL, WEATHER_API_KEY } from './api'
 import { useState } from 'react';
 
 
@@ -14,9 +14,9 @@ function App() {
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(' ')
 
-    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_URL_KEY}&units=metric`)
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
 
-    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_URL_KEY}&units=metric`)
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
 
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (res) => {
--- src/api.js
+++ src/api.js
@@ -1,7 +1,10 @@
+
+const GEO_API_KEY = process.env.REACT_APP_API_KEY_ONE
+
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '3e27cb0da3mshbc0944728421b8fp1fc61ejsna726e41a133d',
+		'X-RapidAPI-Key': GEO_API_KEY,
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -9,5 +12,5 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
-export const WEATHER_API_URL_KEY = "8b048ad51ea3db04e91ed0a11a16f3a0"
+export const WEATHER_API_URL_KEY = process.env.REACT_APP_API_KEY_TWO
 
